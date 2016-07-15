f07fff1cbd17da612700df7be95903d6a83bcec3
milaitie <milaitie@oregonstate.edu>
2016-07-14 19:47:19 -0500

    Add files via upload

---
+++ AI/lannisterschema.c
@@ -0,0 +1,95 @@
+/*
+Use these questions if the user starts at:
+	Kingdom of the Rock
+	Kingdom of the Reach
+
+
+if user picks kingdom of the north
+	if user answers >=3 questions correctly
+		Crossing into enemy territory can be dangerous, especially when the North is a much different 
+		climate than that in the South. Unfortunately, your soldiers could not withstand the winter 
+		and you lost 1,000 men. 
+
+	if user answers 3 > questions correctly 
+		Crossing into enemy territory can be dangerous, especially when the North is a much different 
+		climate than that in the South. Unfortunately, the North are very fierce and were able to 
+		coordinate a suprise attack which wiped out 3,000 of your troops. 
+
+
+if user picks kingdom of the mountain and vale
+	if user answers >= 3 questions correctly
+		Lady Arryn is convinced that the Lannisters killed her husband and therefore has a strong hatred 
+		for anyone who supports the Lannisters. Due to your allegance, she recinds her offer of troops 
+		by 2,000 men and lets you leave without any harm.
+
+	if user answers 3 > questions correctly
+		Lady Arryn is convinced that the Lannisters killed her husband and therefore has a strong hatred 
+		for anyone who supports the Lannisters. Due to your allegance and your poor knowledge of the 
+		area, your troops get trapped in the mountains and 4,000 of your men perish. 
+
+if user picks kingdom of the rock (if user is from the reach)
+	if user answers >= 3 questions correctly
+		Your allegence with the Lannisters has been well established through marraige. Although you still 
+		are cautious about the secrecy and treachery from the Lannister group, they offer you another 
+		2,000 to support your fight against the North. 
+
+	if user answers 3 > questions correctly
+		Although you are allies with the Lannisters of Casterly Rock, they are unsure of your ability to 
+		defeat the North. They still support your fight but don't provide you with additional men to 
+		conquer the North.
+
+if user picks kingdom of the stormlands
+	if user answers <= 3 questions correctly
+		On your way to the Stormlands, your men are exhausted and several have major wounds that have 
+		become infected. Due to their weakness, many men have become unable to fight and need to be sent 
+		home or have died. As a result, 1,000 men have be lost. 
+
+	if user answers 3 > questions correctly
+		On your way to the Stormlands, your men are exhausted and several have major wounds that have 
+		become infected. Due to their weakness, many men have become unable to fight and need to be sent 
+		home or have died. As a result, 1,000 men have be lost. The Brotherhood without Banners that are 
+		in the Stormlands are aware of the situation and have been tracking your progression. The 
+		Brotherhood attack your men, resulting in another 2,000 to be lost. 
+
+
+if user picks kingdom of isles and rivers
+	if user answers 3 <= questions correctly
+		The kingdom of isles and rivers have been striving for their independence and the fight against 
+		the North is meaningless to them. They will give the troops as promised but demand that you allow 
+		them to be independent after the war is won. 
+
+	if user answers 3 > questions correctly
+		The Iron Born are a proud people and know that your allegance with the Lannisters (and Martells 
+		if from the Reach) will result in treachery. Their knowledge of the your devious schemes have 
+		resulted in them not only recinding their offer to suport your claim but they also destroy 50 of 
+		your ships causing you to lose 2,000 men. 
+
+if user picks kingdom of the reach (if user is from the rock)
+	if user answers 3 <= questions correctly
+		Your allegance to the lannisters has proved fruitful and they have offered to give you an additional 
+		3,000 troops.
+
+	if user answers 3 > questions correctly
+		Although your alliance with the Lannisters is strong, they are uncertain of your ability to defeat 
+		the North.  They will support your war but will not offer any additional troops, in case you fail 
+		they will be able to defend themselves. 
+
+
+then the user needs to pick whatever city is left (Dorne)
+	if user answers 3 <= questions correctly
+		The Dornish are very strongly against the Lannisters, however kind they may occasionally come across. 
+		Due to Tywin Lannister commanding for Elia Martell and her children's death, the Dornish are waiting 
+		to seek their revenge. With your presence, you have stirred up very strong feelings for revenge and 
+		the Dornish have acted out. As a result, 2,000 of your men have been slaughtered by the Dornish. The 
+		troops that were promised are still with you but don't mistake this act as a peace offering. Be careful 
+		what you wish for.
+
+	if user answers 3 > questions correctly
+		The Dornish are very strongly against the Lannisters, however kind they may occasionally come across. 
+		Due to Tywin Lannister commanding for Elia Martell and her children's death, the Dornish are waiting to 
+		seek their revenge. With your presence and poor knowledge of their people, you have stirred up very 
+		strong feelings for revenge and the Dornish have acted out. As a result, 4,000 of your men have been 
+		slaughtered by the Dornish. The troops that were promised are still with you but don't mistake this act 
+		as a peace offering. Be careful what you wish for.
+
+		*/
