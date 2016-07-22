1139440f30cacd2e001a63c724f0d553bded40f3
wylie99998 <guerraj@onid.oregonstate.edu>
2016-07-22 19:26:24 -0400

    early save handler for game
    added cookie and input out features for checking client cookie against saved game files

---
+++ save_handler.js
@@ -0,0 +1,66 @@
+//early save funciton for game, checks client for cookie, if its there checks 
+//the data in the cookie against the saved file, if it checks out ask if client
+// wants to load, else, save data and cookie for new client.  
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+var fs = require("fs");
+var fileName = "userdata.txt";
+
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+if(data.includes(user) == true){
+	//ask to load save state
+}
+else {
+	buffer += user;
+	fs.writeFile(fileName,buffer, function(err){
+		if(err){
+			return 0;
+		}
+	});
+	}
